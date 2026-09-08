903fcde03f7f1386a1a7414423614a2386ac9168
Adrian Pascu <adrian@pascu.be>
2026-09-08 03:56:24 +0200

    Bump the version to 1.3.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "be.pascu.mapsforpebble"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
     }
 
     signingConfigs {
